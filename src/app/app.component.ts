9dee4b76b78b4a01130e29db8cc635ebbbd1380f
Marianne Lapierre <LApierre.marianne@gmail.com>
2023-07-10 16:38:01 +0200

    git init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<h1>Git basics</h1>
+  <ul>
+    <li>Git init</li>
+  </ul>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
